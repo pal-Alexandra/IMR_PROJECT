65496a8262024e5cdcfa46aeb5ba323cdadda501
Pal Alexandra <palalexandra3@yahoo.com>
2023-12-04 09:32:57 +0200

    created 4 UI screens

---
+++ Virtual Eminescu Roundabout/Assets/Scripts/BCU_link.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BCU_link : MonoBehaviour
+{
+    public void OpenWikiPage()
+    {
+        Application.OpenURL("https://en.wikipedia.org/wiki/Central_University_Library_of_Ia%C8%99i");
+    }
+
+}
+ 
