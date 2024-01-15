37fc8995576b3f108e6a399addc61b6467592150
Stefan <scatiru1231@gmail.com>
2024-01-15 23:25:21 +0200

    Made street lamps actually light the area and added sample script for UI to see if all buttons are detected

---
+++ Virtual Eminescu Roundabout/Assets/Project Assets/Scripts/XRUIController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XRUIController : MonoBehaviour
+{
+    public Button[] buttons;
+    public Slider volumeSlider;
+
+    void Start()
+    {
+        foreach (Button button in buttons)
+        {
+            button.onClick.AddListener(() => ButtonClicked(button));
+        }
+
+        volumeSlider.onValueChanged.AddListener(delegate { SliderChanged(); });
+    }
+
+    void ButtonClicked(Button button)
+    {
+        Debug.Log("Pressed Button: " + button.gameObject.name);
+    }
+
+    void SliderChanged()
+    {
+        Debug.Log("Slider Value: " + volumeSlider.value);
+    }
+}
