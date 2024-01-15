5b938da31e85374cdd381c7700bc06b099c2fd64
Stefan <scatiru1231@gmail.com>
2024-01-15 19:44:05 +0200

    Merged the rail infrastructure and updated script into main scene and also now points in route are hidden

--- Virtual Eminescu Roundabout/Assets/Project Assets/Scripts/MovePath.cs
+++ Virtual Eminescu Roundabout/Assets/Project Assets/Scripts/MovePath.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class MovePath : MonoBehaviour
 {
-    // Start is called before the first frame update
     public List<GameObject> points;
     public float speed = 2;
     private float rotationSpeed = 5;
     int index = 0;
     public bool isLoop = true;
-    void Start()
-    {
 
+    private void Update()
+    {
+        MoveObject();
+        RotateObject();
     }
 
     void MoveObject()
@@ -34,7 +35,6 @@ public class MovePath : MonoBehaviour
                 {
                     index = 0;
                     transform.position = points[index].transform.position;
-
                 }
             }
         }
@@ -45,18 +45,10 @@ public class MovePath : MonoBehaviour
         if (index < points.Count - 1)
         {
             Vector3 direction = points[index].transform.position - transform.position;
-            direction.y = 0; // Ensure the rotation is only around the y-axis
+            direction.y = 0;
             Quaternion rotation = Quaternion.LookRotation(direction);
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
         }
     }
-
-    private void Update()
-    {
-        MoveObject();
-        RotateObject();
-    }
-    // Update is called once per frame
-
 }
 
